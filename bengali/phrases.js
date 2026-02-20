626fb681f5a2ad0b7074173d1bcf66e32e778ae2
Robi Rahman <robirahman94@gmail.com>
2026-02-20 17:05:03 -0500

    Refactor Bengali app into domain modules with ESM bootstrap

--- bengali/phrases.js
+++ bengali/phrases.js
@@ -1150,3 +1150,6 @@ const PHRASES_DATA = [
     english: "Half, double",
   },
 ];
+
+
+export { PHRASES_SITUATIONS, PHRASES_WAVE_ORDER, PHRASES_DATA };
---
+++ bengali/ui-utils.js
@@ -0,0 +1,17 @@
+function escapeStr(s) {
+  return String(s).replace(/&/g,'&amp;').replace(/</g,'&lt;').replace(/>/g,'&gt;').replace(/"/g,'&quot;').replace(/'/g,'&#39;');
+}
+
+
+function escapeHTML(str) {
+  const d = document.createElement('div');
+  d.textContent = str;
+  return d.innerHTML;
+}
+
+
+function escHtml(s) {
+  return String(s).replace(/&/g,'&amp;').replace(/</g,'&lt;').replace(/>/g,'&gt;').replace(/"/g,'&quot;');
+}
+
+export { escapeStr, escapeHTML, escHtml };
